606dedb948d80001eb05f77dca31a9ea08721f2e
Caseynorth182 <your_email@whatever.comcaseynorth182@gmail.com>
2020-07-09 22:15:44 +0300

    added rateYo plugin

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,22 @@
 $(function () {
+    //slick slider
     $('.slider-inner').slick({
         prevArrow: "<button class='slick-arrow slick-prev'><img src='img/tempSlider/left.svg'></button>",
         nextArrow: "<button class='slick-arrow slick-next'><img src='img/tempSlider/right.svg'></button>"
     })
 
+    // MixitUp 
     var mixer = mixitup('.product__inner');
 
-
+    //dropdown menu toggle
     $('.title-block__btn').on('click', function () {
         $('.dropdown').toggleClass('dropdown--active');
+    });
+
+    //rateYo stars
+    $(".rate-star").rateYo({
+        rating: 4.5,
+        starWidth: "13px",
+        readOnly: true,
     });
 });
