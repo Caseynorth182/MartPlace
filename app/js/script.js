4dbacb46c845498a1de4503fddcc644c631e7e63
Caseynorth182 <your_email@whatever.comcaseynorth182@gmail.com>
2020-07-09 18:31:24 +0300

    added product section and add Mixitup pluggin

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,13 @@
 $(function () {
+    $('.slider-inner').slick({
+        prevArrow: "<button class='slick-arrow slick-prev'><img src='img/tempSlider/left.svg'></button>",
+        nextArrow: "<button class='slick-arrow slick-next'><img src='img/tempSlider/right.svg'></button>"
+    })
 
+    var mixer = mixitup('.product__inner');
+
+
+    $('.title-block__btn').on('click', function () {
+        $('.dropdown').toggleClass('dropdown--active');
+    });
 });
