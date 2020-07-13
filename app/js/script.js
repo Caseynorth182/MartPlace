ead46717272abf7aafd70598df8ca6836d156304
Caseynorth182 <your_email@whatever.comcaseynorth182@gmail.com>
2020-07-13 12:08:01 +0300

    added card-slider,score,and benefits sections

--- app/js/script.js
+++ app/js/script.js
@@ -4,8 +4,21 @@ $(function () {
         prevArrow: "<button class='slick-arrow slick-prev'><img src='img/tempSlider/left.svg'></button>",
         nextArrow: "<button class='slick-arrow slick-next'><img src='img/tempSlider/right.svg'></button>",
         autoplay: true,
+        slidesToShow: 1,
         autoplaySpeed: 2000,
     })
+
+    //card-slider
+    $('.card-slider__inner').slick({
+        prevArrow: "<button class='slick-arrow slick-prev'><img src='img/tempSlider/left.svg'></button>",
+        nextArrow: "<button class='slick-arrow slick-next'><img src='img/tempSlider/right.svg'></button>",
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        variableWidth: true,
+    });
 
     // MixitUp 
     if ($('.products__block').length) {
@@ -24,4 +37,6 @@ $(function () {
         starWidth: "13px",
         readOnly: true,
     });
+
+
 });
