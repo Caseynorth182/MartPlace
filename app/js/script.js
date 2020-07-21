ff6934eeb004c3b32492c21beecade24c913adc9
Caseynorth182 <your_email@whatever.comcaseynorth182@gmail.com>
2020-07-22 00:16:33 +0300

    add dropdown items and adaptive

--- app/js/script.js
+++ app/js/script.js
@@ -6,6 +6,7 @@ $(function () {
         nextArrow: "<button class='slick-arrow slick-next'><img src='img/tempSlider/right.svg'></button>",
         autoplay: true,
         slidesToShow: 1,
+        adaptiveHeight: true,
         autoplaySpeed: 2000,
     })
 
@@ -19,14 +20,35 @@ $(function () {
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
-        variableWidth: true,
+        responsive: [{
+                breakpoint: 1010,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 780,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
     //feedback slider
     $('.feedback-card').slick({
         arrows: true,
         infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
         slidesToShow: 2,
         slidesToScroll: 2,
+        responsive: [{
+            breakpoint: 1000,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }]
     });
 
     // MixitUp 
@@ -39,6 +61,18 @@ $(function () {
     $('.title-block__btn').on('click', function () {
         $('.dropdown').toggleClass('dropdown--active');
     });
+
+    //account-info toogle 
+    $('.header-top__button').on('click', function () {
+        $('.dropmenu__lists').toggleClass('dropmenu__lists--active');
+    });
+    //burger-menu
+    $('.burger').on('click', function () {
+        $('.menu__list').toggleClass('menu__list--active');
+        $('.burger__item').toggleClass('burger__item--active');
+    });
+
+
 
     //rateYo stars
     $(".rate-star").rateYo({
