7cc316144d98dc7dc1f05dd3ce533bd60cd98736
Caseynorth182 <your_email@whatever.comcaseynorth182@gmail.com>
2020-08-03 17:37:11 +0300

    adaptive category,favorite,single-prod,blog v2 pages

--- app/js/script.js
+++ app/js/script.js
@@ -1,61 +1,67 @@
 $(function () {
     //slick slider
-    $('.slider').slick({
-        appendArrows: $('.title-block--blue'),
-        prevArrow: "<button class='slick-arrow slick-prev'><img src='img/tempSlider/left.svg'></button>",
-        nextArrow: "<button class='slick-arrow slick-next'><img src='img/tempSlider/right.svg'></button>",
+    $(".slider").slick({
+        appendArrows: $(".title-block--blue"),
+        prevArrow:
+            "<button class='slick-arrow slick-prev'><img src='img/tempSlider/left.svg'></button>",
+        nextArrow:
+            "<button class='slick-arrow slick-next'><img src='img/tempSlider/right.svg'></button>",
         /*  autoplay: true, */
         slidesToShow: 1,
         autoplaySpeed: 2000,
-        responsive: [{
+        responsive: [
+            {
                 breakpoint: 700,
                 settings: {
                     slidesToShow: 1,
-                    arrows: false
-                }
+                    arrows: false,
+                },
             },
-
-        ]
-    })
+        ],
+    });
 
     //card-slider
-    $('.card-slider__inner').slick({
-        appendArrows: $('.title-block--purple'),
-        prevArrow: "<button class='slick-arrow slick-prev'><img src='img/tempSlider/left.svg'></button>",
-        nextArrow: "<button class='slick-arrow slick-next'><img src='img/tempSlider/right.svg'></button>",
-        /*  autoplay: true, */
+    $(".card-slider__inner").slick({
+        appendArrows: $(".title-block--purple"),
+        prevArrow:
+            "<button class='slick-arrow slick-prev'><img src='img/tempSlider/left.svg'></button>",
+        nextArrow:
+            "<button class='slick-arrow slick-next'><img src='img/tempSlider/right.svg'></button>",
+        autoplay: true,
         autoplaySpeed: 2000,
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
-        responsive: [{
+        responsive: [
+            {
                 breakpoint: 1010,
                 settings: {
-                    slidesToShow: 2
-                }
+                    slidesToShow: 2,
+                },
             },
             {
                 breakpoint: 780,
                 settings: {
                     slidesToShow: 1,
-                    arrows: false
-                }
-            }
-        ]
+                    arrows: false,
+                },
+            },
+        ],
     });
     //feedback slider
-    $('.feedback-card').slick({
+    $(".feedback-card").slick({
         arrows: true,
         infinite: true,
-        /*    autoplay: true, */
+        autoplay: true,
         autoplaySpeed: 2000,
         slidesToShow: 2,
         slidesToScroll: 2,
-        responsive: [{
+        responsive: [
+            {
                 breakpoint: 1210,
                 settings: {
                     arrows: false,
-                }
+                },
             },
             {
                 breakpoint: 1000,
@@ -63,34 +69,30 @@ $(function () {
                     slidesToShow: 1,
                     slidesToScroll: 1,
                     arrows: false,
-                }
-            }
-        ]
+                },
+            },
+        ],
     });
-
-
-
 
     //dropdown menu toggle
-    $('.title-block__btn').on('click', function () {
-        $('.dropdown').toggleClass('dropdown--active');
+    $(".title-block__btn").on("click", function () {
+        $(".dropdown").toggleClass("dropdown--active");
     });
 
-    //account-info toogle 
-    $('.header-top__button').on('click', function () {
-        $('.dropmenu__lists').toggleClass('dropmenu__lists--active');
+    //account-info toogle
+    $(".header-top__button").on("click", function () {
+        $(".dropmenu__lists").toggleClass("dropmenu__lists--active");
     });
     //burger-menu
-    $('.burger').on('click', function () {
-        $('.menu__list').toggleClass('menu__list--active');
-        $('.burger__item').toggleClass('burger__item--active');
+    $(".burger").on("click", function () {
+        $(".menu__list").toggleClass("menu__list--active");
+        $(".burger__item").toggleClass("burger__item--active");
     });
 
-    //category-filter btn 
+    //category-filter btn
     $(".product-filter__group-btn--list").on("click", function () {
         $(".card-item").addClass("list");
         $(".card-item").removeClass("grid");
-
     });
     $(".product-filter__group-btn--grid").on("click", function () {
         $(".card-item").addClass("grid");
@@ -100,34 +102,46 @@ $(function () {
     //favorite-filter btn
     $(".product-filter__group-btn--list").on("click", function () {
         $(".product-card__item").addClass("product-card__item--list");
-
     });
     $(".product-filter__group-btn--grid").on("click", function () {
         $(".product-card__item").removeClass("product-card__item--list");
     });
-
+    //adaptive btn  grid/list section
+    $(window).resize(function () {
+        if ($(window).width() <= 850)
+            $(".product-card__item").removeClass("product-card__item--list");
+    });
+    if ($(window).width() <= 850)
+        $(".product-card__item").removeClass("product-card__item--list");
 
     //aside accordion category toogle
     $(".accordion-item__trigger").click(function () {
-        $(this).next(".accordion-item__content").slideToggle()
-    })
+        $(this).next(".accordion-item__content").slideToggle();
+    });
 
     //PRODUCT-ONE TABS
-    $('.product-one__tabs .tab').on('click', function (event) {
-        var id = $(this).attr('data-id');
-        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
-        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
-        $(this).addClass('active');
-        $('#' + id).addClass('active-tab').fadeIn();
+    $(".product-one__tabs .tab").on("click", function (event) {
+        var id = $(this).attr("data-id");
+        $(".product-one__tabs ")
+            .find(".tab-item")
+            .removeClass("active-tab")
+            .hide();
+        $(".product-one__tabs .tabs  ").find(".tab").removeClass("active");
+        $(this).addClass("active");
+        $("#" + id)
+            .addClass("active-tab")
+            .fadeIn();
         return false;
     });
     //PRODUCT-ONE TABS
-    $('.blog__tabs .tab').on('click', function (event) {
-        var id = $(this).attr('data-id');
-        $('.blog__tabs').find('.tab-item').removeClass('active-tab').hide();
-        $('.blog__tabs .tabs').find('.tab').removeClass('active');
-        $(this).addClass('active');
-        $('#' + id).addClass('active-tab').fadeIn();
+    $(".blog__tabs .tab").on("click", function (event) {
+        var id = $(this).attr("data-id");
+        $(".blog__tabs").find(".tab-item").removeClass("active-tab").hide();
+        $(".blog__tabs .tabs").find(".tab").removeClass("active");
+        $(this).addClass("active");
+        $("#" + id)
+            .addClass("active-tab")
+            .fadeIn();
         return false;
     });
 
@@ -145,25 +159,23 @@ $(function () {
         force_edges: false, // force UI in the box
         hide_min_max: false, // show/hide MIN and MAX labels
         hide_from_to: false, // show/hide FROM and TO labels
-        block: false // block instance from changing
+        block: false, // block instance from changing
     });
 
-
     //mixItUp category
-    if ($('.product-page').length) {
-        var mixer = mixitup('.product-page', {
-            "animation": {
-                "duration": 250,
-                "nudge": true,
-                "reverseOut": true,
-                "effects": "fade scale(1)"
-            }
+    if ($(".product-page").length) {
+        var mixer = mixitup(".product-page", {
+            animation: {
+                duration: 250,
+                nudge: true,
+                reverseOut: true,
+                effects: "fade scale(1)",
+            },
         });
-
     }
-    // MixitUp 
-    if ($('.product__inner').length) {
-        var mixer = mixitup('.product__inner');
+    // MixitUp
+    if ($(".product__inner").length) {
+        var mixer = mixitup(".product__inner");
     }
     //rateYo stars
     $(".rate-star").rateYo({
@@ -173,6 +185,4 @@ $(function () {
         normalFill: "#FFFFFF",
         readOnly: true,
     });
-
-
 });
